ed7db6c58a94aed1325816c66cdb5979d382e23c
Gino Carlo Juliano <julianogino@yahoo.com>
2017-02-25 20:07:33 +0800

    refactor

--- JUnitTest/src/com/sample/tests/EmployeeTestSuite.java
+++ JUnitTest/src/com/sample/tests/EmployeeTestSuite.java
@@ -1,11 +1,8 @@
-package com.sample.util;
+package com.sample.tests;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
-import com.sample.tests.EmployeeSalaryManagerTest;
-import com.sample.tests.EmployeeValidatorUtilTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ EmployeeSalaryManagerTest.class,
