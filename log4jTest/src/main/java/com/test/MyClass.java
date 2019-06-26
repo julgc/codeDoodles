e93ae7e4747f386ad7265cb75953f4fc85b46189
Gino Carlo Juliano <Gino Carlo Juliano@192.168.254.107>
2019-06-26 08:46:38 +0800

    update

--- log4jTest/src/main/java/com/test/MyClass.java
+++ log4jTest/src/main/java/com/test/MyClass.java
@@ -4,13 +4,15 @@ import org.apache.log4j.Logger;
 
 public class MyClass {
 
-	private static final Logger logger=Logger.getLogger(MyClass.class);
+	private static final Logger logger = Logger.getLogger(MyClass.class);
+
 	public static void main(String[] args) {
 		logger.info("this is an info");
+		logger.debug("this is an debug");
 		try {
 			throw new Exception("Error Intentionally Thrown");
 		} catch (Exception e) {
-			logger.error("this is an ERROR",e);
+			logger.error("this is an ERROR", e);
 		}
 	}
 
